ea98ed921bfc3098f58b849305d29ceca4ed7977
Сергій Гололобов <gololobov.serhiy@gmail.com>
2026-01-15 20:45:21 +0200

    Добавьте файлы проекта.

---
+++ P51_CPP/FileName.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<Windows.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	/*cout << "Hello C++" << endl;
+	cout << 1234 << endl;
+	cout << "Мене звати Сергій" << endl;*/
+
+	// type name;
+
+	// bool  - 1b
+
+	// char  - 1b
+
+	// short - 2b
+	// int   - 4b
+	// long  - 4b
+	// long long - 8b
+
+	// float - 4b (7)
+	// double - 8b (15)
+	// long doble - 10b 
+
+	/*int a;
+	int b = 5;
+	a = 10;
+	cout << &a << endl;*/
+
+	/*int c = 5.99f;
+	cout << c << endl;*/
+
+	unsigned int a = -1;
+	cout << a << endl;
+
+	const int b = 22;
+	//b = 10;
+
+
+
+
+
+
+
+	system("pause");
+}
